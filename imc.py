f6f802c140041ce8878d1c6ae6b96cd172cb5442
Karen Limari <karenlimari20@gmail.com>
2024-07-26 20:17:35 +0200

    varibales, formulas y output inicial

---
+++ imc.py
@@ -0,0 +1,22 @@
+#Actividad 1 - IMC
+# El IMC, conocido también como el Índice de masa corporal, es una medida que asocia el peso de una persona con su talla (su altura). Este valor es utilizado normalmente como un indicador nutricional y constituye un índice fácil y sencillo de calcular para determinar el estado de obesidad y sobrepeso de una persona. El IMC se calcula de la siguiente manera:
+
+#IMC = W / H^2
+
+#W : corresponde al peso de la persona en Kg.
+#H: corresponde a la altura en metros.
+#IMC: EL valor del IMC, en [Kg/m2]
+#Para ello, la Organización Mundial de la Salud (OMS) ha determinado una clasificación así para distintos rangos de valores.
+
+#IMC Clasificación OMS
+#< 18.5 Bajo Peso
+#[ 18.5, 25 [ Adecuado
+#[ 25, 30 [ Sobrepeso
+#[ 30, 35[ Obesidad Grado I
+#[ 35, 40 [ Obesidad Grado II
+#> 40 Obesidad Grado III
+
+#En busca de mejorar la salud nutricional de los pacientes, se le solicita a usted como programador el hecho de diseñar una herramienta que permita determinar el estado nutricional de una persona.
+
+#Paso 1 
+#Determinar variables
